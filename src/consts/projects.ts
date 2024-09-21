364e23b695e59a688eb8618e7370bf3805fda301
Darren Xu <darrenxu1994@gmail.com>
2024-09-21 14:25:55 +1000

    initial commit

---
+++ src/consts/projects.ts
@@ -0,0 +1,145 @@
+export const projects = [
+  {
+    name: "Civic lunch",
+    preview: "https://lunch.darrenxu.com/",
+    description: "Astro website with Notion API backend, hosted for free",
+    link: "https://github.com/DarrenXu94/new-civic-lunch",
+    imgLink:
+      "https://github.com/user-attachments/assets/83836305-7c04-4cbe-a264-12846877f64a",
+  },
+  {
+    name: "Civic lunch alpha",
+    preview: "https://civic-lunch.darrenxu.com/",
+    description: "A foodie's journey through lunch",
+    link: "https://github.com/DarrenXu94/awake-template",
+    imgLink:
+      "https://github.com/DarrenXu94/awake-template/assets/16931153/6735407b-1caa-4d3a-b642-1944c82f3938",
+  },
+  {
+    name: "Wordle animal ripoff",
+    preview: "https://animalle.darrenxu.com/",
+    description: "A wordle ripoff with animals",
+    link: "https://github.com/DarrenXu94/animalle",
+    imgLink:
+      "https://github.com/user-attachments/assets/b2c4db32-4d4c-4684-b97d-b1899116d1a9",
+  },
+  {
+    name: "Cookbook using Notion API",
+    preview: "https://cookbook.darrenxu.com/",
+    description: "A cookbook site using the Notion API",
+    link: "https://github.com/DarrenXu94/vue-cookbook",
+    imgLink:
+      "https://github.com/user-attachments/assets/c1f1b99f-1e5f-4536-8a2e-ee56ad2f6274",
+  },
+  {
+    name: "Stocky timeline",
+    preview: "https://www.stocky.darrenxu.com/",
+    description: "A timeline of the Stocky sharehouse (RIP)",
+    link: "https://github.com/DarrenXu94/vue-stocky",
+    imgLink:
+      "https://github.com/DarrenXu94/vue-stocky/assets/16931153/34314548-d38c-4da3-9839-14a7bce10c35",
+  },
+  {
+    name: "Futsal scrape",
+    preview: "https://futsal.darrenxu.com/",
+    description: "Scraping a poorly built website for my own use",
+    link: "https://github.com/DarrenXu94/astro-dribl",
+    imgLink:
+      "https://github.com/DarrenXu94/astro-dribl/assets/16931153/2b16e29a-c443-4623-91cd-6c4843514a65",
+  },
+  {
+    name: "Public API Displayer",
+    preview: "https://publicapi.darrenxu.com/",
+    description: "A site with a list of public free to use APIs",
+    link: "https://github.com/DarrenXu94/public-api-displayer",
+    imgLink:
+      "https://user-images.githubusercontent.com/16931153/152439824-e5c4938b-46b3-481e-9484-5381b9889b53.png",
+  },
+  {
+    name: "Spritesheet Cropper",
+    preview: "https://spritesheet-cropper.vercel.app/",
+    description:
+      "NextJS app that cuts a single spritesheet into individual images",
+    link: "https://github.com/DarrenXu94/spritesheet-cropper",
+    imgLink:
+      "https://user-images.githubusercontent.com/16931153/154889161-407b1040-2c15-48c9-8148-416af6ce60c7.png",
+  },
+  {
+    name: "ThreeJS Portfolio",
+    preview: "https://three.darrenxu.com/",
+    description: "A demo site I made to test the skills I learn with ThreeJS",
+    link: "https://github.com/DarrenXu94/portfolio-three",
+    imgLink:
+      "https://user-images.githubusercontent.com/16931153/153305187-925194ac-78dc-4833-93fc-ea992a2183f6.gif",
+  },
+  {
+    name: "LottieWeb Scroll",
+    preview: "https://lottietest.darrenxu.com/",
+    description:
+      "A demo site exploring LottieFiles, GSAP and scroll animations",
+    link: "https://github.com/DarrenXu94/lottie-testing",
+    imgLink:
+      "https://user-images.githubusercontent.com/16931153/153337587-6a08bd36-fbd4-40af-881a-39301803acf2.gif",
+  },
+  {
+    name: "Framer Motion Page Transition",
+    preview: "https://framer-transition.darrenxu.com/",
+    description: "Framer Motion page transition swipe effect",
+    link: "https://github.com/DarrenXu94/framer-page-transition",
+    imgLink:
+      "https://user-images.githubusercontent.com/16931153/153304883-8810675f-9553-4ce0-b3ab-915ce859a7a5.gif",
+  },
+  {
+    name: "Video Play on Scroll",
+    preview: "https://scroll.darrenxu.com/",
+    description:
+      "Scrolling example using GSAP and TweenMagic to play a video on scroll",
+    link: "https://github.com/DarrenXu94/video-play-on-scroll",
+    imgLink:
+      "https://user-images.githubusercontent.com/16931153/152892113-588e2684-e50f-4d7a-a49e-b188383723a0.gif",
+  },
+  {
+    name: "Choccy Blog",
+    preview: "https://choccy.darrenxu.com/",
+    description:
+      "Hear about my latest hard hitting choccy reviews. A Gatsby blog hosted on Netlify with custom React components and styling",
+    link: "https://github.com/DarrenXu94/gatsby-starter-netlify-cms",
+    imgLink:
+      "https://user-images.githubusercontent.com/16931153/95315560-5fec0f80-08de-11eb-87f0-0890b4dbb3b4.png",
+  },
+  {
+    name: "Will it Cereal?",
+    preview: "https://willitcereal.darrenxu.com/",
+    description:
+      "A fun blog where I see what food goes well as a cereal. Another Gatsby blog using the same tools as the Choccy Blog",
+    link: "https://github.com/DarrenXu94/will-it-cereal",
+    imgLink:
+      "https://user-images.githubusercontent.com/16931153/102003406-3bd40100-3d5b-11eb-8256-d0e6d635f06f.png",
+  },
+  {
+    name: "DBScan Visualisation",
+    description: "CanvasJS representation of the DBScan algorithm",
+    preview: "https://darrenxu94.github.io/dbscan-visualiser/",
+    link: "https://github.com/DarrenXu94/dbscan-visualiser",
+    imgLink:
+      "https://user-images.githubusercontent.com/16931153/121803918-fac79580-cc86-11eb-91a0-c3a548ce45d8.png",
+  },
+
+  {
+    name: "Beam Game",
+    preview: "https://darrenxu94.github.io/Beam/",
+    description: "A p5js game created for a fun side project",
+    link: "https://github.com/DarrenXu94/Beam",
+    imgLink:
+      "https://user-images.githubusercontent.com/16931153/50622683-585a4280-0f63-11e9-944c-88d82733d9ba.gif",
+  },
+  {
+    name: "Web developer roadmap",
+    preview: "https://roadmapdxu.netlify.app/",
+    description:
+      "Explore a comprehensive web developer roadmap that lists essential technologies, outlining their basic functions and roles in web development. Perfect for beginners and professionals looking to stay updated.",
+    link: "https://github.com/DarrenXu94/roadmap",
+    imgLink:
+      "https://user-images.githubusercontent.com/16931153/50617009-2931da00-0f3f-11e9-9d06-d2cf2f6daf20.png",
+  },
+];
